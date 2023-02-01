1e1b97fd7fe1a5703e337cfe2a0e10b64d1f1cb6
이경범 <tkdwk567@naver.com>
2023-02-01 20:08:58 +0900

    Initial commit

---
+++ src/main/java/com/example/springpetclinicclone/SpringPetclinicCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpetclinicclone/SpringPetclinicCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinicclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
